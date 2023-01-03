1da756ec682960d2873b2e713e4ba60064708089
Nagy Zétény <nagyzeteny6@gmail.com>
2023-01-03 13:42:49 +0100

    Removed Meta Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,206 +1,45 @@
 import type { NextPage } from 'next';
-import type { Props as User } from '@components/User';
-import { useState } from 'react';
-import { Meta } from '@components/Meta';
-import { questions, levels } from '@constants/questions';
-import { Form } from '@components/User';
-import { EmailPopup, LevelFailedPopup } from '@components/Popup';
-import { trpc } from '@utils/trpc';
+import Head from 'next/head';
+import { Content } from '@components/Content';
 
-const Home: NextPage = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [answers, setAnswers] = useState<Record<string, number>>({});
-  const [level, setLevel] = useState<number | null>(null);
-  const [isDone, setIsDone] = useState<boolean>(false);
-  const [isEmailPopupOpen, setIsEmailPopupOpen] = useState<boolean>(false);
-  const [isLevelFailedPopupOpen, setIsLevelFailedPopupOpen] = useState<boolean>(false);
+const Page: NextPage = () => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico" />
 
-  const currentQuestions = questions.filter((question) => question.level === level);
+      <meta name="author" content="stay" />
+      <meta
+        name="keywords"
+        content="stay, Zétény, Nagy, Zétény Nagy, BMSzC Neumann János Informatikai Technikum, Matek, Math, Maths, Quiz"
+      />
 
-  const { mutate, isLoading, isSuccess } = trpc.email.send.useMutation({
-    onMutate: () => setIsEmailPopupOpen(true),
-  });
+      <meta property="og:locale" content="hu_HU" />
+      <meta property="og:type" content="website" key="og_type" />
+      <meta property="og:site_name" content="Matek - Zétény Nagy" key="site_name" />
 
-  const handleNextLevel = () => {
-    if (level && level < levels) {
-      if (
-        currentQuestions.filter((question) => question.correct === answers[question.id]).length < 2
-      )
-        return setIsLevelFailedPopupOpen(true);
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="znagy.hu" />
 
-      window.scrollTo({ top: 0 });
-      setLevel(level + 1);
-    }
+      <meta name="robots" content="index, follow" />
+      <meta name="language" content="English" />
+      <meta name="rating" content="general" />
 
-    if (level === levels) setIsDone(true);
-  };
+      <title>Matek - Zétény Nagy</title>
+      <meta property="og:title" content="Matek - Zétény Nagy" key="title" />
+      <meta name="twitter:title" content="Matek - Zétény Nagy" />
 
-  if (!user) {
-    return (
-      <>
-        <Meta path="/" title="Matek - Zétény Nagy" desc="Matek - Zétény Nagy" />
+      <meta name="description" content="Matek - Zétény Nagy" />
+      <meta property="og:description" content="Matek - Zétény Nagy" />
+      <meta name="twitter:description" content="Matek - Zétény Nagy" />
 
-        <main>
-          <Form setUser={setUser} />
-        </main>
-      </>
-    );
-  }
+      <meta name="url" content="https://matek.znagy.hu/" />
+      <meta property="og:url" content="https://matek.znagy.hu/" />
+      <meta property="twitter:url" content="https://matek.znagy.hu/" />
+    </Head>
 
-  if (!level) {
-    return (
-      <>
-        <Meta path="/" title="Matek - Zétény Nagy" desc="Matek - Zétény Nagy" />
+    <Content />
+  </>
+);
 
-        <main className="mx-auto flex min-h-screen w-11/12 flex-col items-center justify-center">
-          <div className="flex w-full max-w-lg flex-col gap-4 rounded-2xl bg-neutral-800 p-8 text-sm shadow-2xl sm:p-12 md:px-20">
-            <h2 className="text-xl font-bold text-neutral-50">Szabályok, játék menete</h2>
-            <p>
-              Az utolsó szint kivételével, minden szinten 3 kérdés található. Ha legalább 2 kérdésre
-              jól válaszol tovább jut következő szintre. Ha nem, akkor újra próbálhatja az utolsó
-              szintet, vagy befelyezhezi a játékot.
-            </p>
-
-            <button
-              className="group flex w-full items-center rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 font-medium  text-white hover:from-green-400 hover:to-blue-600 hover:text-white"
-              type="button"
-              onClick={() => setLevel(1)}
-            >
-              <span className="flex w-full items-center justify-center rounded-md bg-neutral-800 px-6 py-3 transition-all group-hover:bg-opacity-0">
-                Rendben
-              </span>
-            </button>
-          </div>
-        </main>
-      </>
-    );
-  }
-
-  if (isDone) {
-    const answeredQuestions = questions.filter((question) => question.level <= level);
-    const correctAnswers = answeredQuestions.filter(
-      (question) => question.correct === answers[question.id],
-    ).length;
-
-    return (
-      <>
-        <Meta path="/" title="Matek - Zétény Nagy" desc="Matek - Zétény Nagy" />
-
-        <main>
-          <section className="mx-auto flex min-h-screen w-11/12 flex-col items-center justify-center">
-            <div className="flex w-full max-w-lg flex-col gap-4 rounded-2xl bg-neutral-800 p-8 text-sm shadow-2xl sm:p-12 md:px-20">
-              <h2 className="text-xl font-bold text-neutral-50">Köszönjük a játékot!</h2>
-              <ul>
-                <li>
-                  Ön a(z) <b>{level}.</b> szintig jutott el <b>{levels}</b> szintből.
-                </li>
-                <li>
-                  Eredmény: <b>{((correctAnswers / answeredQuestions.length) * 100).toFixed(2)}%</b>
-                </li>
-                <li>
-                  Összesen <b>{answeredQuestions.length}</b> kérdésből <b>{correctAnswers}</b>{' '}
-                  helyes választ adott meg.
-                </li>
-              </ul>
-
-              <p>
-                Amennyiben szeretné, hogy válaszait, az ön által megadott e-mail címre elküldjük,
-                nyomja meg az alábbi gombot.
-              </p>
-
-              <button
-                className="group flex w-full items-center rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 font-medium  text-white hover:from-green-400 hover:to-blue-600 hover:text-white"
-                type="button"
-                onClick={() => mutate({ user, answers, level })}
-              >
-                <span className="flex w-full items-center justify-center rounded-md bg-neutral-800 px-6 py-3 transition-all group-hover:bg-opacity-0">
-                  Kérem az eredményt!
-                </span>
-              </button>
-            </div>
-          </section>
-
-          <EmailPopup
-            isOpen={isEmailPopupOpen}
-            setIsOpen={setIsEmailPopupOpen}
-            isLoading={isLoading}
-            isSuccess={isSuccess}
-          />
-        </main>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Meta path="/" title="Matek - Zétény Nagy" desc="Matek - Zétény Nagy" />
-
-      <main>
-        {currentQuestions.map((question, index) => (
-          <section
-            key={question.id}
-            id={question.id}
-            className="mx-auto flex min-h-screen w-11/12 flex-col items-center justify-center"
-          >
-            <div className="flex w-full max-w-lg flex-col gap-4 rounded-2xl bg-neutral-800 p-8 text-sm shadow-2xl sm:p-12 md:px-20">
-              <div className="flex flex-col gap-4">
-                <h2 className="text-xl font-medium text-neutral-300">
-                  {(level - 1) * 3 + index + 1}. Kérdés
-                </h2>
-
-                <h3 className="w-fit font-medium text-neutral-300">{question.question}</h3>
-
-                <div className="flex flex-col gap-2">
-                  {question.answers.map((answer, index) => (
-                    <div
-                      key={`${question.id}-${index}`}
-                      className="flex w-fit cursor-pointer items-center gap-2"
-                    >
-                      <input
-                        type="radio"
-                        id={`${question.id}-${index}`}
-                        checked={answers[question.id] === index}
-                        onChange={() => setAnswers({ ...answers, [question.id]: index })}
-                      />
-
-                      <label htmlFor={`${question.id}-${index}`}>{answer}</label>
-                    </div>
-                  ))}
-                </div>
-              </div>
-
-              <button
-                className="group flex w-full items-center rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 font-medium  text-white hover:from-green-400 hover:to-blue-600 hover:text-white"
-                type="button"
-                onClick={
-                  index === currentQuestions.length - 1
-                    ? handleNextLevel
-                    : () =>
-                        document
-                          .getElementById(currentQuestions[index + 1].id)
-                          ?.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                <span className="flex w-full items-center justify-center rounded-md bg-neutral-800 px-6 py-3 transition-all group-hover:bg-opacity-0">
-                  {index === currentQuestions.length - 1
-                    ? level === levels
-                      ? 'Befejezem'
-                      : 'Következő szint'
-                    : 'Következő'}
-                </span>
-              </button>
-            </div>
-          </section>
-        ))}
-
-        <LevelFailedPopup
-          isOpen={isLevelFailedPopupOpen}
-          setIsOpen={setIsLevelFailedPopupOpen}
-          setIsDone={setIsDone}
-        />
-      </main>
-    </>
-  );
-};
-
-export default Home;
+export default Page;
